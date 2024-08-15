44e44b5b329fb41bfc0f89312516ad3b713b3ee6
S.M SIFATUL ISLAM <sifatcoc1234@gmail.com>
2024-08-15 19:27:06 +0600

    Add files via upload

---
+++ handtrackmodule.py
@@ -0,0 +1,85 @@
+import cv2
+import mediapipe as mp
+import time
+
+# Initialize webcam
+class handDetector:
+    
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, tranCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.tranCon = tranCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            static_image_mode=self.mode,
+            max_num_hands=self.maxHands,
+            min_detection_confidence=self.detectionCon,
+            min_tracking_confidence=self.tranCon
+        )
+        self.mpDraw = mp.solutions.drawing_utils
+        self.results = None
+
+    def findHands(self, img, draw=True):
+        # Convert the image to RGB
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB) 
+        
+        # Print the results
+        # print(self.results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+    
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList = []
+        if self.results and self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
+
+        return lmList
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+        
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+            
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (18, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        
+        # Show the image
+        cv2.imshow("Image", img)
+        
+        # Exit the loop when the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    # Release the webcam and close the window
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
